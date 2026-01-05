180928be6456e7323b3d0d03a5700bebf6a33d49
bbdaria <dashayakhliel@gmail.com>
2026-01-05 14:12:38 +0200

    kernel3

--- HW2/filters.py
+++ HW2/filters.py
@@ -123,7 +123,8 @@ def sobel_operator():
 
     #sobel_matrix = [[1,0,-1],[2,0,-2],[1,0,-1]]
     #sobel_matrix = [[3,0,-3],[10,0,-10],[3,0,-3]]
-    sobel_matrix = [[1,0,-1],[2,0,-1],[1,0,-2],[2,0,-2],[1,0,-1]]
+    #sobel_matrix = [[1,0,-1],[2,0,-1],[1,0,-2],[2,0,-2],[1,0,-1]]
+    sobel_matrix = [[1,1,1],[1,0,1],[1,1,1]]
     sobel_filter = np.array(sobel_matrix)
 
     Gx = correlation_numba(sobel_filter, pic)
